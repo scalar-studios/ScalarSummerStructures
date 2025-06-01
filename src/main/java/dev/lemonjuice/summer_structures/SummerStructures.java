2a581ce65f7a681f7e1c25e436c838dd34067882
Lemon-Juiced <47193272+Lemon-Juiced@users.noreply.github.com>
2025-05-31 21:51:43 -0400

    Cleaned up mod for work

---
+++ src/main/java/dev/lemonjuice/summer_structures/SummerStructures.java
@@ -0,0 +1,35 @@
+package dev.lemonjuice.summer_structures;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+
+@Mod(SummerStructures.MODID)
+public class SummerStructures {
+    public static final String MODID = "summer_structures";
+
+    public SummerStructures(IEventBus modEventBus, ModContainer modContainer) {
+        // Basically nothing lives in this file, so its vanilla compatible.
+        modEventBus.addListener(this::commonSetup);
+        NeoForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {}
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {}
+
+    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {}
+    }
+}
